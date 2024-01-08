99864ff0a5ba63835e2f040b6fcf434ce72ad023
Edouard CHIN <chin.edouard@gmail.com>
2024-01-08 16:59:20 +0100

    Add logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def foo
+    Rails.logger.debug("This is a debug message")
+    Rails.logger.info("This is a info message")
+
+    render(body: "Hello world")
+  end
 end
